155faa4784e0d39cd89a4f98c7b0a76727677d55
Daniil Saushkin <0suffix0@gmail.com>
2023-05-08 10:59:01 +0700

    Добавьте файлы проекта.

---
+++ DataGenerator_Core/IGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGenerator_Core
+{
+    interface IGenerator
+    {
+        public int NumberRange(int start, int end);
+        public float FloatRange(float start, float end);
+        public string String(int start, int end);
+        public string Name();
+        public string City();
+        public string Country();
+        public DateTime DateTime(DateTime start, DateTime end);
+    }
+}
---
+++ DataGenerator_Core/Services/SQLGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGenerator_Core.Services
+{
+    internal class SQLGenerator
+    {
+    }
+}
