714a10f672d8e1cdbf1c06a9be1aef4032bc97f8
Daniil Saushkin <0suffix0@gmail.com>
2023-05-14 15:25:06 +0700

    Create entities TEMP and TYPE

---
+++ DataGenerator_Core/Entites/Template.cs
@@ -0,0 +1,10 @@
+﻿namespace DataGenerator_Core.Entites
+{
+    public sealed class Template
+    {
+        public int ID { get; set; }
+        public string? Data { get; set; }
+        public int TypeID { get; set; }
+        public Type? Type { get; set; }
+    }
+}
---
+++ DataGenerator_Core/Entites/Type.cs
@@ -0,0 +1,9 @@
+﻿namespace DataGenerator_Core.Entites
+{
+    public sealed class Type
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public List<Template> Templates { get; set; } = new(); 
+    }
+}
