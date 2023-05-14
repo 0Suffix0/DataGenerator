5781ed6e86a8c5654f984d63308f4dd5e335640b
Daniil Saushkin <0suffix0@gmail.com>
2023-05-14 16:18:59 +0700

    Hello world!

--- DataGenerator_CLI/Program.cs
+++ DataGenerator_CLI/Program.cs
@@ -1,33 +1,11 @@
-﻿using DataGenerator_Core.Entites;
-using DataGenerator_Core.Services;
-
-namespace DataGenerator_CLI
+﻿namespace DataGenerator_CLI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Column firstColumn = new Column();
-            firstColumn.ColumnName = "FirstAge";
-            firstColumn.Type = "NumberRange";
-            firstColumn.From = 20;
-            firstColumn.To = 40;
+            Console.WriteLine("Hello world!");
 
-            Column secondColumn = new Column();
-            secondColumn.ColumnName = "DoubleRange";
-            secondColumn.Type = "DoubleRange";
-            secondColumn.From = 5;
-            secondColumn.To = 10;
-
-            List<Column> columns = new List<Column>();
-            columns.Add(firstColumn);
-            columns.Add(secondColumn);
-
-            Controller controller = new ();
-
-            string result = controller.Generator(columns, "SQL", 5);
-
-            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
