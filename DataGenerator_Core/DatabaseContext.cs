e276c62fd1333d2238da014632cc24462d25b76d
Daniil Saushkin <0suffix0@gmail.com>
2023-05-14 15:29:23 +0700

    Create DatabaseContext.cs

---
+++ DataGenerator_Core/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using DataGenerator_Core.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataGenerator_Core
+{
+    public sealed class DatabaseContext : DbContext
+    { 
+        private readonly string? connectionString = System.Configuration.ConfigurationManager.AppSettings.Get("connectionString");
+
+        public DbSet<Entites.Type> Types { get; set; } = null!;
+        public DbSet<Template> Templates { get; set; } = null!;
+
+        public DatabaseContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
