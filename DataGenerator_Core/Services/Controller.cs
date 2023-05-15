fe234f91b5c3608fced529c9bc985564037d641f
Daniil Saushkin <0suffix0@gmail.com>
2023-05-15 20:34:41 +0700

    Add comment and rename method

--- DataGenerator_Core/Services/Controller.cs
+++ DataGenerator_Core/Services/Controller.cs
@@ -1,11 +1,23 @@
 ﻿using DataGenerator_Core.Entites;
-using System.Collections;
 
 namespace DataGenerator_Core.Services
 {
     public sealed class Controller
     {
-        public string Generator(List<Column> columns, string toConvert, int counter)
+        private readonly string? _connectionString;
+
+        public Controller(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Starts generating test data
+        /// </summary>
+        /// <param name="columns"></param>
+        /// <param name="counter"></param>
+        /// <returns>String converted to the selected format</returns>
+        public string GenerateData(List<Column> columns, string toConvert, int counter)
         {
             if (columns == null) 
                 throw new ArgumentNullException("Column is null");
@@ -14,7 +26,7 @@ namespace DataGenerator_Core.Services
                 if (column.Type == null || column.ColumnName == null) 
                     throw new ArgumentException("Required fields is null");
 
-            Generator generator = new ();
+            Generator generator = new (_connectionString);
             List<List<string>> generatorResult = generator.Start(columns, counter);
 
             Converter converter = new ();
