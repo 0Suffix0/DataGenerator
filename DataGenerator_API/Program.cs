5785ba12cc3765f7bb8c123e9335695230e629dc
Daniil Saushkin <0suffix0@gmail.com>
2023-05-27 20:35:47 +0700

    Create first API

--- DataGenerator_API/Program.cs
+++ DataGenerator_API/Program.cs
@@ -1,15 +1,46 @@
+using DataGenerator_Core.Entites;
+using DataGenerator_Core.Services;
+
 namespace DataGenerator_API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            string _connectionString = System.Configuration.ConfigurationManager.AppSettings["connectionString"];
+            Controller controller = new Controller(_connectionString);
+
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.Run(async (context) =>
+            {
+                var response = context.Response;
+                var request = context.Request;
+                if (request.Path == "/api/DataGenerator/")
+                {
+                    string ConvertTo = context.Request.Query["ConvertTo"];
+                    int count = Convert.ToInt32(context.Request.Query["count"]);
+                    List<Column> columns = new List<Column>();
 
-            app.Run();
+                    try
+                    {
+                        columns = await request.ReadFromJsonAsync<List<Column>>();
+                    }
+                    catch
+                    {
+                        columns.Add(await request.ReadFromJsonAsync<Column>());
+                    }
+
+                    if (columns != null)
+                    {
+                        var result = controller.GenerateData(columns, ConvertTo, count);
+                        await context.Response.WriteAsync(result);
+                    }
+                }
+            });
+
+            app.Run("http://localhost:3000");
         }
     }
 }
