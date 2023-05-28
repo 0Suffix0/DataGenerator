c6f4f239bfb63a379e2545cbd306ca864bdedc20
Daniil Saushkin <0suffix0@gmail.com>
2023-05-28 10:08:24 +0700

    Create API

--- DataGenerator_API/Program.cs
+++ DataGenerator_API/Program.cs
@@ -1,5 +1,3 @@
-using DataGenerator_Core.Entites;
-using DataGenerator_Core.Services;
 
 namespace DataGenerator_API
 {
@@ -7,40 +5,17 @@ namespace DataGenerator_API
     {
         public static void Main(string[] args)
         {
-            string _connectionString = System.Configuration.ConfigurationManager.AppSettings["connectionString"];
-            Controller controller = new Controller(_connectionString);
-
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
             var app = builder.Build();
 
-            app.Run(async (context) =>
-            {
-                var response = context.Response;
-                var request = context.Request;
-                if (request.Path == "/api/DataGenerator/")
-                {
-                    string ConvertTo = context.Request.Query["ConvertTo"];
-                    int count = Convert.ToInt32(context.Request.Query["count"]);
-                    List<Column> columns = new List<Column>();
+            app.UseHttpsRedirection();
+            app.MapControllers();
 
-                    try
-                    {
-                        columns = await request.ReadFromJsonAsync<List<Column>>();
-                    }
-                    catch
-                    {
-                        columns.Add(await request.ReadFromJsonAsync<Column>());
-                    }
-
-                    if (columns != null)
-                    {
-                        var result = controller.GenerateData(columns, ConvertTo, count);
-                        await context.Response.WriteAsync(result);
-                    }
-                }
-            });
-
-            app.Run("http://localhost:3000");
+            app.Run();
         }
     }
 }
