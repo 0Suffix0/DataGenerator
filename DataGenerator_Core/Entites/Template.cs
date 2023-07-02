91a157e18ddb98286fb688a62bb8f2de9d6cbe3f
Daniil Saushkin <0suffix0@gmail.com>
2023-07-02 19:36:20 +0700

    feature/CreateEntitiesControllers #4
    feat: Изменил схему сериализации JSON.

--- DataGenerator_Core/Entites/Template.cs
+++ DataGenerator_Core/Entites/Template.cs
@@ -1,10 +1,17 @@
-﻿namespace DataGenerator_Core.Entites
+﻿using System.Text.Json.Serialization;
+
+namespace DataGenerator_Core.Entites
 {
     public sealed class Template
     {
         public int ID { get; set; }
         public string? Data { get; set; }
         public int TypeID { get; set; }
+
+        [JsonIgnore]
         public Type? Type { get; set; }
+
+        [JsonPropertyName("type")]
+        public string? TypeName { get { return Type?.Name; } }
     }
 }
